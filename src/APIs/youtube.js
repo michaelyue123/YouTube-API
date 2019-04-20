af76435dc850e1cb8bcb1cdd5038b6fc3b1dbc56
Michael Yue <45374025+michaelyue123@users.noreply.github.com>
2019-04-20 12:55:51 +1000

    Update youtube.js

--- src/APIs/youtube.js
+++ src/APIs/youtube.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 // Axios is a promise-based HTTP client that works both 
-//in the browser and in a node.js environment. It provides 
-//a single API for dealing with XMLHttpRequest s and node's http interface. 
-//It also wraps the requests using a polyfill for ES6 new's promise syntax.
+// in the browser and in a node.js environment. It provides 
+// a single API for dealing with XMLHttpRequest s and node's http interface. 
+// It also wraps the requests using a polyfill for ES6 new's promise syntax.
 
 const KEY = 'AIzaSyDEMZGJp1dCtCHMmK_MOsG7ePKcjEXYfOs';
 
